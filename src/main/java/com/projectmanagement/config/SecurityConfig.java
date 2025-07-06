4508d49d2f8db125e3ba236ad9b830837e1c9784
avinesh <avineshsit@gmail.com>
2025-07-06 17:21:17 +0530

    added auth2 dependency

--- src/main/java/com/projectmanagement/config/SecurityConfig.java
+++ src/main/java/com/projectmanagement/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Component;
 
 @EnableMethodSecurity
 @Configuration
+@EnableWebSecurity
 @Component
 public class SecurityConfig {
 
